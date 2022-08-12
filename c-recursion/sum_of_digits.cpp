1f6d5f70db439afee28a2a24e23580a2ba387ae0
spykid-111 <rrsingh1101@gmail.com>
2022-08-13 01:32:20 +0530

    done sod

---
+++ c-recursion/sum_of_digits.cpp
@@ -0,0 +1,29 @@
+// To find the sum of digits 
+#include<bits/stdc++.h>
+using namespace std;
+
+int rec_sod(int n)
+{
+    if(n==0)
+        return 0;
+    return (rec_sod(n/10)+ (n%10));
+}
+int it_sod(int n)
+{
+    int sum = 0;
+    while(n)
+    {
+        sum = sum + (n%10);
+        n/=10;
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << rec_sod(n) << endl;
+    cout << it_sod(n) << endl;
+    return 0;
+}
+// Note: Whenever there is an option of iterative solution we use iterative solution because it will take less time and less auxillary space 
