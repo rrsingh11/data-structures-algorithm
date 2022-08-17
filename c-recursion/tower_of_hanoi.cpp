473d2dd37354cca732f5f2c083514cb8ea832fa7
spykid-111 <rrsingh1101@gmail.com>
2022-08-17 21:52:49 +0530

    done

--- c-recursion/tower_of_hanoi.cpp
+++ c-recursion/tower_of_hanoi.cpp
@@ -9,9 +9,9 @@ void toh(int n, char A, char B, char C)//A is source and C is destination
         cout << "Move " << 1 << " from " << A << " to " << C << endl;
         return;
     }
-    toh(n-1, A, C, B);
+    toh(n-1, A, C, B); //Now n-1 to B till we reach the last
     cout << "Move " << n << " from " << A << " to " << C << endl;
-    toh(n-1,B,A,C);
+    toh(n-1,B,A,C); // Now recursively all the disks of B to C 
 }
 
 int main()
@@ -21,3 +21,13 @@ int main()
     toh(n,A,B,C);   
     return 0;
 }
+/*
+    Output:-
+        Move 1 from p to r
+        Move 2 from p to q
+        Move 1 from r to q
+        Move 3 from p to r
+        Move 1 from q to p
+        Move 2 from q to r
+        Move 1 from p to r
+*/
